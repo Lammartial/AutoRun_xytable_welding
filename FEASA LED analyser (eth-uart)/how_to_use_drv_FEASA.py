ecfd08d27fca34c30bb864fb57587cdf59ecdba0
oliginov <oleksandr.liginov@rrc-ps.de>
2022-12-13 15:54:24 +0100

    LED Analyzer has been checked with the custom PCBA.

--- FEASA LED analyser (eth-uart)/how_to_use_drv_FEASA.py
+++ FEASA LED analyser (eth-uart)/how_to_use_drv_FEASA.py
@@ -4,13 +4,17 @@ if __name__ == "__main__":
     from time import sleep
 
     # Use LAN connection and "Net Module Configure" software to check CH9121 parameters:
-    # 1. LAN parameters of CH9121 (IP address, port number)
+    # 1. LAN parameters of CH9121: TCP SERVER mode, PORT1 (IP address, port number1), PORT2 (IP address, port number2)
     # 2. UART parameters of CH9121 (baudrate 57600, Data bits 8, Stop bit 1)
 
     # Check FEASA LED ANALYSER RS232 settings. Default: baudrate 57600, Data bits 8, Stop bit 1
 
     CH9121_IP_STR = "192.168.1.90" 
+    # Channel 1
     CH9121_PORT_STR = 2000
+    # Channel 2
+    #CH9121_PORT_STR = 3000
+
 
     # 1. Create an instance of ITECH_DEV class
     feasa = FEASA_DEV(CH9121_IP_STR, CH9121_PORT_STR)
