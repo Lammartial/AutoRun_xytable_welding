d2355ad9258ab3cf2d66989c2436f8c10138ad0d
mruth <markus.ruth@rrc-ps.de>
2023-01-05 16:45:09 +0100

    added functions to get configs

--- station_config_loader.py
+++ station_config_loader.py
@@ -17,9 +17,25 @@ class StationConfiguration:
 
     def get_station_configuration(self) -> tuple:
         # return the station's configuration in a convenient way for teststand
-        pass
+        order = ["test_type", "station_id", "line_id"]
+        result = tuple([(field, self._CONFIG[field]) for field in order])
+        result += (("test_sockets", len(self._CONFIG["test_sockets"])),)
+        #result = tuple([self._CONFIG[field] for field in order])
+        return result
+
+    def get_resource_strings_for_socket(self, socket: str) -> tuple:
+        # return the selected socket configuration in a convenient way for teststand
+        assert (socket > 0 and socket <= 3), ValueError("Socket must be in [1..3].")
+        d = dict(self._CONFIG["test_sockets"][str(socket)]["resource_strings"])
+        return tuple([(k, v) for k,v in d.items()])
+        #return tuple([self._CONFIG[field] for field in order])
+
 
 cfg = StationConfiguration()
 pprint(cfg._CONFIG)
+
+pprint(cfg.get_station_configuration())
+pprint(cfg.get_resource_strings_for_socket(2))
+
 
 # END OF FILE
