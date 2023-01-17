a723b1b80129c9207fe92ba8fcdcf740b54a7974
mruth <markus.ruth@rrc-ps.de>
2023-01-17 23:34:05 +0100

    clean comments

--- ui/progress_bar.py
+++ ui/progress_bar.py
@@ -42,55 +42,19 @@ class ProgressWindow:
 
         self.root = ROOT
         self.hidden = None
-
-        # # create root tkinter window to hold progress bar
-        # self.root = tkinter.Tk()
-        # self.hide()
-
-        # # Simply set the theme
-        # #self.root.tk.call("source", Path(__file__).resolve().parent / "theme_sv.tcl")
-        # #self.root.tk.call("set_theme", "light")
-
-        # style = ttk.Style()
-        # #print(style.theme_names())
-        # style.theme_use('winnative')
-
-        # #style = ttk.Style()
-        # #print(style.theme_names())
-        # #style.theme_use('winnative')
-        # #try:
-        # #    style.element_create("color.pbar", "from", "winnative")
-        # #except Exception as ex:
-        # #    print(ex)
-        # #    pass
-        # #style.layout("ColorProgress.Horizontal.TProgressbar",
-        # #                    [('Horizontal.Progressbar.trough',
-        # #                    {'sticky': 'nswe',
-        # #                    'children': [('Horizontal.Progressbar.color.pbar', {'side': 'left', 'sticky': 'ns'})]})])
-
         if color and color == "":
             color = None  # TestStand cannot transfer "None"
         if color:
             global style
             style.configure("ColorProgress.Horizontal.TProgressbar", background=color)
-
-
-        #mainframe = ttk.Frame(root, padding="15 15 15 15")
-        #mainframe.pack(fill="both")
-
-        #self.root.attributes('-alpha', 0)  # this hides the root window until we have arranged all the wigets
         self.root.title(title)
-
         # create the Widgets and keep them inside our App object
-        # #self.root.minsize(100, 50)
         x_size = int(self.root.winfo_screenwidth() * 0.60)
         y_size = int(self.root.winfo_screenheight() * 0.1)
         x = int((self.root.winfo_screenwidth() - x_size) / 2)
         y = int((self.root.winfo_screenheight() - y_size) / 2)
         self.root.geometry(f"{x_size}x{y_size}+{x}+{y}")
-        #center(self.root)
-        #Define the geometry for the window or frame
-        #self.root.attributes('-alpha', 1.0)  # now make the main window visible again
+        # define the geometry for the window or frame
         self.root.columnconfigure(0, weight=1)
         # create progress bar
         self.progress = ttk.Progressbar(self.root,
@@ -98,10 +62,8 @@ class ProgressWindow:
                     style="ColorProgress.Horizontal.TProgressbar" if color else None)
         # pack progress bar into root
         self.progress.pack(fill=tkinter.BOTH, expand=1)
-        print("STYLE:", self.progress["style"])
-        #
         # leave window hidden
-        #
+
 
     def hide(self):
         self.root.withdraw()  # hide window
@@ -138,17 +100,13 @@ if __name__ == '__main__':
         LOOP_ACTIVE = True
         while LOOP_ACTIVE:
             print(i)
-            #progress.step(10)
-            #progress.value = 100
             win.set_value(10*i)
-            #win.update()
             time.sleep(0.4)
             i = i + 1
             if (i > mx):
                 win.quit()
                 LOOP_ACTIVE = False
             else:
-                #value_progress = i
                 pass
 
         win.hide()
