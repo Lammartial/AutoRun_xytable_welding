60d316754651184b90e02c5c08f2123efd1ef829
oliginov <oleksandr.liginov@rrc-ps.de>
2023-02-06 17:39:12 +0100

    ...

--- keysight.py
+++ keysight.py
@@ -326,24 +326,24 @@ if __name__ == "__main__":
     res : float = 0
 
     # predefined resource ID
-    DAQ970A_IP_STR = "TCPIP0::192.168.1.53::inst0::INSTR" #"TCPIP0::169.254.196.86::inst0::INSTR"
-    DAQ970A_NAME_STR = "TCPIP0::K-DAQ970A-17481.local::inst0::INSTR"
+    DAQ970A_IP_STR = "TCPIP0::172.21.101.13::inst0::INSTR" #"TCPIP0::169.254.196.86::inst0::INSTR"
+    #DAQ970A_NAME_STR = "TCPIP0::K-DAQ970A-17481.local::inst0::INSTR"
 
     # 1. Create an instance of DAQ970A class
     chn = 0                                 # default value
     daq970a = DAQ970A(DAQ970A_IP_STR, chn)
 
     # 2. Do some stuff
-    print(daq970a.selftest())
+    #print(daq970a.selftest())
 
-    res = daq970a.get_resistance(1,1)
-    print(res)
+    #res = daq970a.get_resistance(1,1)
+    #print(res)
 
     #print(daq970a.get_4w_resistance(1,2))
 
-    print(daq970a.get_VDC(1,3))
+    #print(daq970a.get_VDC(1,3))
 
-    print(daq970a.get_VAC(1,4))
+    #print(daq970a.get_VAC(1,4))
 
     print(daq970a.get_ADC(1,21))
 
