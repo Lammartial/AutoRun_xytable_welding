68052087bade8eb852f76b1f14106544ddb625de
Teststand Development <teststand_develop@rrc-ps.com>
2024-11-10 17:31:42 +0700

    sequences for QSB on mockup

--- dsp/mockup_server.py
+++ dsp/mockup_server.py
@@ -150,14 +150,14 @@ async def get_parameter_for_test_run(test_type, station_id, line_id, test_socket
     #_product_name = "RRC2054-SO"
     #_product_name = "SPINEL"
     #_product_name = "RRC2040-2S"
-    _product_name = "RRC2054-2S"
+    #_product_name = "RRC2054-2S"
     #_product_name = "RRC2054-2-HM"
     #_product_name = "RRC2054-2-LM"
     #_product_name = "QSB2040B"
-    #_product_name = "QSB2054B"
+    _product_name = "QSB2054B"
     #_product_name = "QSB2040-2B"
     #_product_name = "QSB2054-2B"
-   
+
 
     _mock = PART_INFORMATION[_product_name]
 
