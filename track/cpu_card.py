daf03c272d719612015dd4d40c46ec557392b3aa
mruth <markus.ruth@rrc-ps.com>
2025-01-17 14:12:31 +0100

    more TRackCPU functions

--- track/cpu_card.py
+++ track/cpu_card.py
@@ -5,7 +5,7 @@ It implements the Logic CPU components library.
 """
 
 from typing import Tuple
-from struct import pack, unpack, unpack_from
+from time import sleep
 from rrc.eth2serial.base import Eth2SerialDevice, OWN_PRIMARY_IP
 from rrc.serialport import SerialComportDevice
 
@@ -69,7 +69,31 @@ class CPU_Card:
     # Standard SCPI commands
 
     def Ident(self) -> str:
+        """Returns the identification string of the application.
+
+        Returns:
+            str: _description_
+        """
         return self.con.request("*IDN?")
+
+
+    def IdentBoot(self) -> str:
+        """Reads the identification string of the bootloader by switching the CPU into bootloader
+        then read IDN and after that return to the application.
+
+        Returns:
+            str: Identification string of the bootloader.
+        """
+        r1 = self.con.request("*BOO")
+        sleep(0.050)  #  Give CPUcard time to start bootloader and re-init UART
+        res = self.con.request("*IDN?")
+        r2 = self.con.request(f"*RES")
+        sleep(0.050)  # Give CPUcard time to start application and re-init UART
+        return res
+
+
+    def Reset(self) -> str:
+        return self.con.request(f"*RES")
 
 
     #----------------------------------------------------------------------------------------------
@@ -112,6 +136,78 @@ class CPU_Card:
 
     # I2C Slave commands
 
+    def I2C_Slave_setBatterySimulationCfg(self) -> bool:
+        res = self.con.request(f":I2C:SLA:BATSIMulation")
+        return self._is_ok_response(res)
+
+    def I2C_Slave_setAddr(self, slave_address: int) -> bool:
+        res = self.con.request(f":I2C:SLA:ADR {slave_address}")
+        return self._is_ok_response(res)
+
+    def I2C_Slave_setCfg(self, cmd: int, mod: int, cnt: int, bytes_to_send: bytearray) -> bool:
+        """Configurate I2C Slave command entry.
+		Args:
+            cmd (int): Command on the CPU card shell react.
+            mod (int): Read/Write/PEC modes
+            cnt (int): count of bytes 0...8
+            byte (_type_): byte(s) to send on request
+
+        Returns:
+            bool: _description_
+        """
+
+        b = ",".join([int(i) for i in bytes_to_send])
+        req = f":I2C:SLA:CFG {cmd},{mod},{cnt},{b}"
+        res = self.con.request(req)
+        return self._is_ok_response(res)
+
+    def I2C_Slave_getCfg(self) -> str:
+        res = self.con.request(":I2C:SLA:CFG?")
+        return res
+
+    def I2C_Slave_clearAllCmds(self) -> bool:
+        res = self.con.request(f":I2C:SLA:CLE!")
+        return self._is_ok_response(res)
+
+    def I2C_Slave_clearAllRequests(self) -> bool:
+        res = self.con.request(f":I2C:SLA:REQ!")
+        return self._is_ok_response(res)
+
+    def I2C_Slave_CmdRequest(self, cmd: int) -> str:
+        res = self.con.request(f":I2C:SLA:REQ? {cmd}")
+        return res
+
+    def I2C_Slave_enablePEC(self, enable: bool) -> bool:
+        res = self.con.request(f":I2C:SLA:PEC {1 if enable else 0}")
+        return self._is_ok_response(res)
+
+    def I2C_Slave_getPEC(self) -> bool:
+        """get slave PEC status.
+
+        Returns:
+            bool: true if PEC enable, false if not
+        """
+        res = self.con.request(f":I2C:SLA:PEC?")
+        return (res == "1")
+
+
+    #----------------------------------------------------------------------------------------------
+
+    # Global commands
+
+    def I2C_Reset(self) -> bool:
+        """Resets the I2C hardware interface (unblocking lines).
+
+        Returns:
+            bool: _description_
+        """
+        res = self.con.request(f":I2C:RES")
+        return self._is_ok_response(res)
+
+    def CPU_Reset(self) -> bool:
+        res = self.con.request(f"*RES")
+        return self._is_ok_response(res)
+
     #----------------------------------------------------------------------------------------------
 
     # SMB commands (can use any GPIO port!)
@@ -121,8 +217,85 @@ class CPU_Card:
 
     # IO port commands
 
+    def _verify_port_exists(self, port_letter: str) -> None:
+        assert(port_letter in ["A", "C", "F", "D", "E"]), ValueError(f"Error. Port {port_letter} does not exist.")
 
+    def IO_Get_Portstatus(self, port_letter: str) -> str:
+        self._verify_port_exists(port_letter)
+        res = self.con.request(f":IO:P{port_letter}?")
+        return res
 
+    def IO_Set_Cfg(self, port_letter: str, bit: int, cfg: int) -> bool:
+        """Set Configuration for a specific port bit.
+
+        Args:
+            port_letter (str): one of A, C
+            bit (int): port number to set 0..7.
+            cfg (int): 0 input, 1 outputt, 2 output OC, 3 output OC with internal pullup
+        Returns:
+            bool: true on success, fail on any error.
+        """
+
+        self._verify_port_exists(port_letter)
+        assert(bit < 8), ValueError(f"Error. Bit paramater '{bit}' not in 0..7.")
+        assert(cfg < 4), ValueError(f"Error. Cfg paramater '{cfg}' not in 0..3.")
+        res = self.con.request(f":IO:P{port_letter}:CFG b{bit}={cfg}")
+        return self._is_ok_response(res)
+
+    def IO_Set_Cfg(self, port_letter: str, cfg: int) -> bool:
+        """Set all Bits of a specific port to the same configuration.
+
+        Args:
+            port_letter (str): one of A, C
+            cfg (int): 0 input, 1 outputt, 2 output OC, 3 output OC with internal pullup
+
+        Returns:
+            bool: true on success, fail on any error.
+        """
+        self._verify_port_exists(port_letter)
+        assert(cfg < 4), ValueError(f"Error. Cfg paramater '{cfg}' not in 0..3.")
+        res = self.con.request(f":IO:P{port_letter}:CFG {','.join([f'b{i}={cfg}' for i in range(8)])}")
+        return self._is_ok_response(res)
+
+    def IO_Get_Cfg(self, port_letter: str) -> str:
+        """Get the configuration of a Port.
+
+        Args:
+            port_letter (str): One of A, C, F, D or E
+
+        Returns:
+            str: returns the configuration of the whole port
+        """
+        self._verify_port_exists(port_letter)
+        res = self.con.request(f":IO:P{port_letter}:CFG?")
+        return res
+
+    def IO_Set_Port(self, port_letter: str, value: int) -> bool:
+        """Set a whole Port with a defined byte/word depending on the port size.
+
+        Args:
+            port_letter (str): One of A, C, F, D or E
+            value (int): _description_
+
+        Returns:
+            bool: _description_
+        """
+        self._verify_port_exists(port_letter)
+        res = self.con.request(f":IO:P{port_letter} 0x{f'{value:02x}'.upper()}")
+        return self._is_ok_response(res)
+
+    def IO_Read_Port_bit(self, port_letter: str, bit: int) -> int:
+        self._verify_port_exists(port_letter)
+        res = self.con.request(f":IO:P{port_letter}:IN {bit}")
+        return int(res)
+
+    def IO_Write_Port_bit(self, port_letter: str, bit: int, value: int) -> bool:
+        self._verify_port_exists(port_letter)
+        assert(bit < 8), ValueError(f"Error. Bit paramater '{bit}' not in 0..7.")
+        res = self.con.request(f":IO:P{port_letter}:O b{bit}={value}")
+        return self._is_ok_response(res)
+
+    #----------------------------------------------------------------------------------------------
 
 
 
---
+++ track/logic_cpu_components.py
@@ -0,0 +1,25 @@
+"""
+This is the Python remake of our TRack' Logic CPU Components C# driver .DLLs.
+
+It implements only IO, I2C and SMB classes and thus is more like "LogicCPUComponentsLIGHT" lib.
+
+"""
+
+from typing import Tuple
+
+
+
+
+class IO:
+    pass
+
+
+class I2C:
+    pass
+
+class SMB:
+    pass
+
+
+
+# END OF FILE
