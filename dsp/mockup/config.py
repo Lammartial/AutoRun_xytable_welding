bb009cbc7179e12f2c8699b86a959edaecc4a9f0
mruth <markus.ruth@rrc-ps.com>
2025-03-26 00:50:46 +0100

    removed old mockup_server.py

--- dsp/mockup/config.py
+++ dsp/mockup/config.py
@@ -6,13 +6,19 @@ Configuration module for mockup server. Please edit here, not in mockup_server.p
 #
 # Mockup server port is 8000 by default. It is selectable by the .CMD batch file so you
 # can easily configure more than one server to run in parallel on this PC by adding
-# --port parameter on the call.
-# E.g.
-#   uvicorn mockup_server:app --reload --host 0.0.0.0 --port 8011
-#   uvicorn mockup_server:app --reload --host 0.0.0.0 --port 8022
-#   uvicorn mockup_server:app --reload --host 0.0.0.0 --port 8033
+# --port parameter on the call:
 #
-# Then you need to set this port in the station_config.yaml of a workstation of a line.
+# Example:
+#   uvicorn mockup.server:app --host 0.0.0.0 --port 8011
+#   uvicorn mockup.server:app --host 0.0.0.0 --port 8022
+#   uvicorn mockup.server:app --host 0.0.0.0 --port 8033
+#
+#   Please not that there is no --reload parameter so you can change the CONFIGURED_PRODUCT
+#   before calling each mockup -> on TSDEV we can run 3 mockups for the lines that way!
+#
+#
+# Besides that  you need also to set this port in the station_config.yaml of a
+# workstation under test or used for rework on a line.
 #
 #--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
@@ -45,17 +51,18 @@ CONFIGURED_PRODUCT = "RRC2040B"
 #
 # This is to configure the printer for mockup_information if used on rework more easily
 #
-
+#
 SELECTED_PRINTER_LOCATION = "VN_GENERIC"  # "VN_GENERIC", "DE_LINE_1", ..., etc.
-
+#
 # set to 1 to allow mockup server to trigger label print for all products, set 0 to disable
 ENABLE_LABEL_PRINTING = 1
-
+#
 # set to 1 to allow entry in label .DAT file otherwise set to 0
 DO_PRINT_SINGLEBOX_LABEL = 1
 DO_PRINT_HARDPACK_LABEL = 1
-
+#
+#
 #--------------------------------------------------------------------------------------------------
-
-
+#
+#
 # END OF FILE
