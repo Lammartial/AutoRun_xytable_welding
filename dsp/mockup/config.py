66dd21d4c4084b0646e0123034774173524e29ff
mruth <markus.ruth@rrc-ps.com>
2025-03-26 01:32:49 +0100

    description moved from comment to docstring

--- dsp/mockup/config.py
+++ dsp/mockup/config.py
@@ -1,24 +1,24 @@
 """
 Configuration module for mockup server. Please edit here, not in mockup_server.py anymore.
+
+Mockup server port is 8000 by default. It is selectable by the .CMD batch file so you
+can easily configure more than one server to run in parallel on this PC by adding
+--port parameter on the call:
+
+Example:
+  uvicorn mockup.server:app --host 0.0.0.0 --port 8011
+  uvicorn mockup.server:app --host 0.0.0.0 --port 8022
+  uvicorn mockup.server:app --host 0.0.0.0 --port 8033
+
+  Please not that there is no --reload parameter so you can change the CONFIGURED_PRODUCT
+  before calling each mockup -> on TSDEV we can run 3 mockups for the lines that way!
+
+
+Besides that  you need also to set this port in the station_config.yaml of a
+workstation under test or used for rework on a line.
+
 """
 
-#--------------------------------------------------------------------------------------------------
-#
-# Mockup server port is 8000 by default. It is selectable by the .CMD batch file so you
-# can easily configure more than one server to run in parallel on this PC by adding
-# --port parameter on the call:
-#
-# Example:
-#   uvicorn mockup.server:app --host 0.0.0.0 --port 8011
-#   uvicorn mockup.server:app --host 0.0.0.0 --port 8022
-#   uvicorn mockup.server:app --host 0.0.0.0 --port 8033
-#
-#   Please not that there is no --reload parameter so you can change the CONFIGURED_PRODUCT
-#   before calling each mockup -> on TSDEV we can run 3 mockups for the lines that way!
-#
-#
-# Besides that  you need also to set this port in the station_config.yaml of a
-# workstation under test or used for rework on a line.
 #
 #--------------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------------
